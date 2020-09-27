ed66fd6d1e5114f2519e659fef601ba037f57ef5
Jbristthuille <jbristhuille@gmail.com>
2020-09-27 13:02:06 +0200

    # Hud
    * Adding player life HUD

---
+++ Weekend_Jam_2/Assets/Hud/Scipts/life.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class life : MonoBehaviour {
+  public int cond = 0;
+
+  private player_stats ps;
+
+  // Start is called before the first frame update
+  void Start() {
+    ps = GameObject.Find("Player").GetComponent<player_stats>();
+  }
+
+  // Update is called once per frame
+  void Update() {
+    if (ps.pv < cond) {
+      gameObject.SetActive(false);
+    }
+  }
+}
