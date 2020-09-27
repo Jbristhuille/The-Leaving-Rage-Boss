ce42064119f9144bb999a4da1b53fe93e4bb4175
Jbristthuille <jbristhuille@gmail.com>
2020-09-27 16:23:34 +0200

    # Final boss
    * Adding final boss mechanisms

---
+++ Weekend_Jam_2/Assets/Enemies/Scripts/final_boss.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class final_boss : MonoBehaviour {
+  public GameObject firePoint;
+  public GameObject firePrefab;
+
+  public float couldownAction;
+  public float speed;
+  public int damage;
+
+  private Rigidbody2D rb;
+  private int action = 0;
+  private float cd;
+  private bool ready = true;
+  private enemies_stats stats;
+
+  // Start is called before the first frame update
+  void Start() {
+    rb = gameObject.GetComponent<Rigidbody2D>();
+    cd = couldownAction;
+    stats = gameObject.GetComponent<enemies_stats>();
+  }
+
+  // Update is called once per frame
+  void Update() {
+    if (stats.pv <= 0)
+      Destroy(gameObject);
+
+    if (cd <= 0 && ready) {
+      action = Random.Range(-1f, 1f) > 0 ? 1 : 2;
+
+      Debug.Log(action);
+
+      ready = false;
+      if (action == 1) { // Rush
+        rb.velocity = new Vector2(speed*gameObject.transform.localScale.x, 0);
+      } else if (action == 2) { // Launch objects
+        cd = couldownAction;
+        ready = true;
+        Instantiate(firePrefab, firePoint.transform.position, Quaternion.identity);
+      }
+    } else {
+      cd -= Time.deltaTime;
+    }
+  }
+
+  void OnTriggerEnter2D(Collider2D col) {
+    rb.velocity = new Vector2(0, 0);
+    Vector3 newScale = gameObject.transform.localScale;
+    newScale.x = newScale.x * -1;
+    gameObject.transform.localScale = newScale;
+    ready = true;
+    cd = couldownAction;
+
+    if (col.gameObject.tag == "Player") {
+      player_stats ps = GameObject.Find("Player").GetComponent<player_stats>();
+      ps.pv -= damage;
+    }
+  }
+}
---
+++ Weekend_Jam_2/Assets/Enemies/Scripts/launch_object.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class launch_object : MonoBehaviour {
+    private int damage = 1;
+    private float fireSpeed = 15;
+    private float lifetime = 2;
+    private Rigidbody2D rb;
+    private player_stats ps;
+
+    // Start is called before the first frame update
+    void Start() {
+      ps = GameObject.Find("Player").GetComponent<player_stats>();
+      rb = gameObject.GetComponent<Rigidbody2D>();
+      int direction = GameObject.Find("Final_boss").transform.localScale.x > 0 ? 1 : -1;
+
+      rb.velocity = new Vector2(fireSpeed * direction, 0);
+    }
+
+    // Update is called once per frame
+    void Update() {
+      if (lifetime > 0) {
+        lifetime -= Time.deltaTime;
+      } else {
+        Destroy(gameObject);
+      }
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+      if (col.gameObject.tag == "Player") {
+        player_stats ps = GameObject.Find("Player").GetComponent<player_stats>();
+        ps.pv -= damage;
+        Destroy(gameObject);
+      }
+    }
+}
